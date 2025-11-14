37f48ca12c1f52f595a01cb06e11cc9df69e5769
chiyo2suke <munkhtushig123@gmail.com>
2025-11-14 16:08:19 +0800

    start

---
+++ index.js
@@ -0,0 +1,48 @@
+// Require express and body-parser
+const express = require("express")
+const bodyParser = require("body-parser")
+const axios = require("axios")
+
+// Initialize express and define a port
+const app = express()
+const PORT = 3000
+const apiUrl = 'https://www.sumo-api.com/api/webhook/test?type=endBasho';
+const body = {
+  "name": "webhookTest",
+  "destination": "https://chiyotasuke-webhook.vercel.app/hook",
+  "secret": "chiyotasukeWantsWebhook",
+  "subscriptions": {
+    "endBasho": true
+  }
+};
+
+app.use(express.json());
+// Set up a simple view engine (e.g., EJS) for rendering HTML
+
+app.get('/hook', (req, res) => {
+  axios.post(apiUrl, body)
+  .then(response => {
+    // Handle successful response
+    console.log('Success:', response.data);
+    res.send("Webhook requested")
+  })
+  .catch(error => {
+    // Handle errors
+    console.error('Error:', error);
+  });
+})
+app.use(express.json());
+app.post('/hook', (req, res) => {
+  const receivedData = req.body;
+
+  console.log("Headers:")
+  console.dir(req.headers);
+  console.log("\nBody:")
+  console.dir(receivedData);
+  res.status(204).send("I received payload");
+});
+
+// Start express on the defined port
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+})
